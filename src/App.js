9f473e16642375e98a0e13148616d5b5a95c89b9
konstantin-kh <kospomazan@gmail.com>
2019-07-02 00:44:04 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Item from './Item'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor (props){
+    super(props)
+    this.state = {
+      todos: []
+    }
+  }
+  
+  componentDidMount() {
+    this.fetchTodos()
+      .then((todos) => this.setState({todos}))
+  }
+
+  fetchTodos() {
+    return fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(res => res.json())
+      .then(json => json.slice(0, 5))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ul>
+          {this.state.todos.map((todo)=> 
+            <Item text={todo.title}/>
+          )}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
